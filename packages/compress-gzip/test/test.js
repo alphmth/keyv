51e2a8cd19e99c64ad75c512e103317631e5ed8f
alphmth <>
2022-09-01 21:50:52 +0500

    support pako instead of node-gzip

--- packages/compress-gzip/test/test.js
+++ packages/compress-gzip/test/test.js
@@ -6,7 +6,7 @@ test('gzip compression/decompression', async t => {
 	const compressed = await keyv.compress('whatever');
 	t.not(compressed, 'whatever');
 	const decompressed = await keyv.decompress(compressed);
-	t.is(decompressed.toString(), 'whatever');
+	t.is(decompressed, 'whatever');
 });
 
 // Test serialize compression
@@ -23,32 +23,4 @@ test('deserialize compression', async t => {
 	const json = await serialize({value: 'whatever'});
 	const djson = await deserialize(json);
 	t.deepEqual(djson, {expires: undefined, value: 'whatever'});
-});
-// Test options while compress
-test('options while compress', async t => {
-	const keyv = new KeyvGzip();
-	const compressed = await keyv.compress('whatever', {chunkSize: 32 * 1024});
-	t.not(compressed, 'whatever');
-	const compressedWithoutOptions = await keyv.compress('whatever');
-	t.not(compressed, compressedWithoutOptions);
-});
-// Test options while decompress
-test('options while decompress', async t => {
-	const keyv = new KeyvGzip();
-	const compressed = await keyv.compress('whatever', {chunkSize: 32 * 1024});
-	t.not(compressed, 'whatever');
-	const compressedWithoutOptions = await keyv.compress('whatever');
-	t.not(compressed, compressedWithoutOptions);
-	const decompress = await keyv.decompress(compressed);
-	const decompressWithoutOptions = await keyv.decompress(compressedWithoutOptions);
-	t.not(decompress, decompressWithoutOptions);
-	t.is(decompress.toString(), decompressWithoutOptions.toString());
-});
-// Test options at class level
-test('options at class level', async t => {
-	const keyv = new KeyvGzip({chunkSize: 32 * 1024});
-	const compressed = await keyv.compress('whatever');
-	t.not(compressed, 'whatever');
-	const compressedWithoutOptions = await new KeyvGzip().compress('whatever');
-	t.not(compressed, compressedWithoutOptions);
 });
